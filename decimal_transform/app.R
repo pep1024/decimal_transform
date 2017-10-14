a7273572668ca6f28fcc982a18eb81e883b2766d
Pep Porra <josepmporra@gmail.com>
2017-10-14 23:21:41 +0200

    Create canonical reactive behavior and use of a button to launch fraction computation

--- decimal_transform/app.R
+++ decimal_transform/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(schoolmath)
+source("global.R")
 
 ui <- fluidPage(
   
@@ -9,7 +10,8 @@ ui <- fluidPage(
     Given a decimal number a.b(c), where c represents the repeating part or repetend, it can be expressed as a fraction.
     Decimal numbers that have non zero repetend are called repeating or recurring decimals.
     If they do not have repetend, they are called terminating decimals"),
-  p("This application computes the fraction expression of a given decimal number"),
+  p("This application finds the fraction expression of a given decimal number.
+    Build first the decimal number by introducing each part a, b and c.\n Then Click the button \'Compute fraction\'"),
   
   
   # Sidebar with textboxes to get number info
@@ -22,6 +24,7 @@ ui <- fluidPage(
       textInput("rec_part", label = "Recurrent part (c)") 
     ),
     mainPanel(
+      actionButton("go", "Compute fraction"),
       h4("Equivalent fraction"),
       h4(textOutput("y"))
     )
@@ -34,31 +37,40 @@ shinyServer <- function(input, output) {
   
   ### write here reactive functions
   ## Compute decimal number x from a, b, c
-  
-  output$x <- renderText({
-    ## Builds the decimal number from the inputs parts
-    x <- ifelse(input$int_part == "", 0, as.integer(input$int_part))
+  create_num <- reactive({
+    x <- as.character(
+      ifelse(input$int_part == "", 0, as.integer(input$int_part)))
     m <- as.integer(nchar(input$rec_part))
+    y <- input$dec_part
+    n <- as.integer(nchar(input$dec_part))
+    z <- input$rec_part
+    
+    if (m == 0 && n == 0) {
+      resp <- x
+    } else {
+      resp <- paste0(x, ".", y, 
+        ifelse(m > 0, paste0("(", input$rec_part, ")"), "")) 
+    }
     
     # Generate the decimal number from the text input
-    recurrent_char <- ifelse(m > 0, paste0("(", input$rec_part, ")"), "")
-    paste0("a.b(c) = ", as.character(x), ".", input$dec_part, recurrent_char)
+    num <- c(resp, x, y, z)
+    names(num) <- c("decimal", "a", "b", "c")
+    num
   })
   
-  output$y <- renderText({
-    # Find the equivalent fraction
-    x <- ifelse(input$int_part == "", 0, as.integer(input$int_part))
-    n <- as.integer(nchar(input$dec_part))
-    y <- input$dec_part
-    m <- as.integer(nchar(input$rec_part))
-    z <- input$rec_part
+  create_fraction <- eventReactive(input$go,{
+    dec <- create_num()
     # Generate the decimal number from the text input
-    x1 <- as.integer(paste0(as.character(x), y, z))
-    x2 <- as.integer(paste0(as.character(x), y))
+    x1 <- as.integer(paste(dec[-1], collapse = ""))
+    x2 <- as.integer(paste(dec[2:3], collapse = ""))
+    n <- nchar(dec[3]) 
+    m <- nchar(dec[4])
+    #x1 <- as.integer(paste0(as.character(x), y, z))
+    #x2 <- as.integer(paste0(as.character(x), y))
     num_char <- as.character(ifelse(m == 0, x2, x1 - x2))
     den_char <- as.character(ifelse(m == 0, 10^n, 10^n * (10^m - 1)))
     
-    resp <- paste0("x = ", num_char, " / ", den_char)
+    resp <- paste0(dec[1], " = ", num_char, " / ", den_char)
     
     num <- as.numeric(num_char)
     den <- as.numeric(den_char)
@@ -71,8 +83,18 @@ shinyServer <- function(input, output) {
     resp3 <- ""
     if (den / MCD == 1) resp3 <- paste0(" = ", as.character(num / MCD))  
     
-    paste0(resp, resp2, resp3)
-    
+    ifelse(dec[1] == "0", "", paste0(resp, resp2, resp3))
+  })
+  
+  
+  output$x <- renderText({
+    ## Builds the decimal number from the inputs parts
+    paste0("a.b(c) = ", create_num()[1])
+  })
+  
+  output$y <- renderText({
+    ## Find the equivalent fraction
+    create_fraction()
   })
 }
 
